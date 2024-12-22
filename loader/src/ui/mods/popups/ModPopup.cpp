57ebd07800f6c64833df643ef5e7dfd876b6c7e2
sheepdotcom <141453189+sheepdotcom@users.noreply.github.com>
2024-12-21 23:16:23 -0500

    yup we still testing random stuff

--- loader/src/ui/mods/popups/ModPopup.cpp
+++ loader/src/ui/mods/popups/ModPopup.cpp
@@ -84,7 +84,8 @@ bool ModPopup::setup(ModSource&& src) {
     auto ptr = reinterpret_cast<uintptr_t>(this);
     auto ptrbtn = reinterpret_cast<uintptr_t>(m_installBtn);
     auto ptrbtn2 = reinterpret_cast<uintptr_t>(&m_installBtn);
-    geode::log::debug("mod popup: {} self {}: install btn: {} 2?: {}", src.getID(), ptr, ptrbtn - ptr, ptrbtn2 - ptr);
+    auto ptrsrc = reinterpret_cast<uintptr_t>(&m_source);
+    geode::log::debug("mod popup: {} self {}: install btn: {} 2?: {} src: {}", src.getID(), ptr, ptrbtn - ptr, ptrbtn2 - ptr, ptrsrc - ptr);
 
     this->setID(std::string(Mod::get()->expandSpriteName(fmt::format("popup-{}", src.getID()))));
 
